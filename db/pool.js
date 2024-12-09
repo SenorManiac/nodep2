6f4ee04fb85982d07a296328fc35d24988f7a39a
SenorManiac <kyle@onlygrandpas.com>
2024-12-08 22:51:24 -0500

    Updating db connection issues

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
+const connectionString = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.DATABASE_PUBLIC_URL;
+
 const config = {
-    host: process.env.PGHOST || 'localhost',
-    user: process.env.PGUSER || 'postgres',
-    password: process.env.PGPASSWORD || 'password',
-    port: process.env.PGPORT || 5432,
-    database: process.env.PGDATABASE || 'railway'
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false, // This is for self-signed certificates. Adjust as needed.
+    }
 };
+
+
 
 module.exports = new Pool(config);
